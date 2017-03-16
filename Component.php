fdef6828b77ed676997b69c92d8926a801df3b78
Nicholas Shipes <nshipes@gmail.com>
2017-03-16 10:43:43 -0700

    add invalid location error handler and template

--- Component.php
+++ Component.php
@@ -102,9 +102,18 @@ class Component {
 		);
 
 		$url = $this->_parse($url, $vars);
-		$content = file_get_contents($url);
-		if ($content) {
-			return $content;
+
+		$ch =  curl_init($url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 3);
+		// curl_setopt($ch, CURLOPT_HTTPHEADER, array('Accept: application/json'));
+		$result = curl_exec($ch);
+
+		if ($result) {
+			return $result;
 		}
 
 		return null;
